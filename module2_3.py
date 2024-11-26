2e8959b3b12a20b95e228d4ea640bb3d906f55cb
Максим <itsuakva41@yandex.ru>
2024-11-26 19:56:09 +1200

    Нужно выписывать из этого списка только положительные числа до тех пор, пока не встретите отрицательное или не закончится список (выход за границу).

--- module2_3.py
+++ module2_3.py
@@ -1,13 +1,12 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 ind = 0
 while True: #Пока правда
-    for i in my_list:
-        if ind < len(my_list):
-            if my_list[ind] > 0:
-                print(my_list[ind])
-                ind = ind + 1
-            elif my_list[ind] == 0 :
-                ind = ind + 1
-                continue
-            else:
-                break
+    for i in range(len(my_list)):
+        if my_list[ind] > 0:
+            print(my_list[ind])
+            ind = ind + 1
+        elif my_list[ind] == 0 :
+            ind = ind + 1
+            continue
+        else:
+            break
