800e18452f072c75aec8e8868e7c6ae0e1ccaf1a
Максим <itsuakva41@yandex.ru>
2024-12-03 21:15:53 +1200

    переделка 3го задания

--- module2_3.py
+++ module2_3.py
@@ -1,11 +1,12 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-ind = 0
-for i in range(len(my_list)):
-    if my_list[ind] > 0:
-        print(my_list[ind])
-        ind += 1
-    elif my_list[ind] == 0 :
-        ind += 1
+
+a = 0
+while a in range(len(my_list)):
+    if my_list[a] > 0:
+        print(my_list[a])
+        a += 1
+    elif my_list[a] == 0 :
+        a += 1
         continue
     else:
         break
